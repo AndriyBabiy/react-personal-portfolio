8f1efbd8d5a5d6d6236870c3904ffb5f00ccfa2f
Andriy Babiy <andriybabiy97@gmail.com>
2024-10-03 22:52:14 +0100

    Base page

---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,12 @@
+import styles from "./ProjectsStyles.module.css";
+
+const Projects = () => {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <a href=""></a>
+    </section>
+  );
+};
+
+export default Projects;
